53d5aa1579052452d924c39428cae282d8a5bc47
MuhammadSufian98 <sufianhassan98@gmail.com>
2025-02-07 14:18:45 +0500

    Create Front page of app

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import FrontPage from "./Components/frontPage/FrontPage";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FrontPage />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    backgroundColor: "#fff",
+    width: "100%",
+    height: "100%",
   },
 });
---
+++ Components/frontPage/FrontPage.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+
+const FrontPage = () => {
+  const [selected, setSelected] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={require("../Logo.png")} style={styles.image} />
+      </View>
+
+      <View style={styles.lang}>
+        <View
+          style={[
+            styles.innerLang,
+            selected === "English" && styles.selectedBorder,
+          ]}
+        >
+          <Pressable
+            style={styles.langContainer}
+            onPress={() => setSelected("English")}
+          >
+            <Image
+              source={require("./united-states.png")}
+              style={styles.langImage}
+            />
+            <Text style={styles.text}>English</Text>
+          </Pressable>
+        </View>
+
+        <View
+          style={[
+            styles.innerLang,
+            selected === "Español" && styles.selectedBorder,
+          ]}
+        >
+          <Pressable
+            style={styles.langContainer}
+            onPress={() => setSelected("Español")}
+          >
+            <Image source={require("./world.png")} style={styles.langImage} />
+            <Text style={styles.text}>Español</Text>
+          </Pressable>
+        </View>
+      </View>
+      <View style={styles.NextOutter}>
+        <View
+          style={
+            selected === "Español" || selected === "English"
+              ? styles.NextBTN
+              : { display: "none" }
+          }
+        >
+          <Pressable>
+            <Text style={styles.nextNext}>Next</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  text: {
+    fontSize: 24,
+    textAlign: "center",
+    margin: 10,
+    fontFamily: "Poppins",
+    fontWeight: "400",
+    color: "#D0AC7B",
+  },
+  imageContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    height: "20%",
+  },
+  image: {
+    width: 70,
+    height: 70,
+  },
+  lang: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "50%",
+    gap: 20,
+  },
+  innerLang: {
+    alignItems: "flex-start",
+    justifyContent: "center",
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: "transparent",
+    width: 219,
+    height: 70,
+  },
+  selectedBorder: {
+    borderColor: "#D0AC7B",
+  },
+  langContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "30%",
+  },
+  langImage: {
+    width: 40,
+    height: 40,
+  },
+  NextOutter: {
+    position: "static",
+    height: "20%",
+    bottom: 0,
+    width: "100%",
+    alignItems: "center",
+  },
+  NextBTN: {
+    backgroundColor: "#D0AC7B",
+    width: 267,
+    height: 77,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "bottom",
+  },
+  nextNext: {
+    fontSize: 24,
+    textAlign: "center",
+    margin: 10,
+    fontFamily: "Poppins",
+    fontWeight: "400",
+    color: "#FFFFFF",
+  },
+});
+
+export default FrontPage;
